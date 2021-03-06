2a3fb777b9356fca32fee9f2b164bce932451a44
Arik Kfir <arik@kfirs.com>
2021-03-06 12:39:35 +0200

    Setup FluxCD

---
+++ tools/flux/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+[[ -z "${CLUSTER_NAME}" ]] || (echo "CLUSTER_NAME is missing" >&2 && exit 1)
+[[ -z "${CLUSTER_ZONE}" ]] || (echo "CLUSTER_ZONE is missing" >&2 && exit 1)
+[[ -z "${GITHUB_TOKEN}" ]] || (echo "GITHUB_TOKEN is missing" >&2 && exit 1)
+[[ -z "${REPO_OWNER}" ]] || (echo "REPO_OWNER is missing" >&2 && exit 1)
+[[ -z "${REPO_NAME}" ]] || (echo "REPO_NAME is missing" >&2 && exit 1)
+[[ -z "${REPO_BRANCH}" ]] || (echo "REPO_BRANCH is missing" >&2 && exit 1)
+[[ -z "${REPO_PATH}" ]] || (echo "REPO_PATH is missing" >&2 && exit 1)
+[[ -z "${NAMESPACE}" ]] || (echo "NAMESPACE is missing" >&2 && exit 1)
+[[ "${NAMESPACE}" == "main" ]] || (echo "Ignoring non-main namespace ('${NAMESPACE}')" >&2 && exit 0)
+
+set -exuo pipefail
+
+gcloud container clusters get-credentials "--zone=${CLUSTER_ZONE}" "${CLUSTER_NAME}"
+if [[ "${NAMESPACE}" == "main" ]]; then
+  flux bootstrap github \
+    --personal \
+    "--owner=${REPO_OWNER}" \
+    "--repository=${REPO_NAME}" \
+    "--branch=${REPO_BRANCH}" \
+    "--path=${REPO_PATH}" \
+    "--namespace=${NAMESPACE}"
+else
+  echo "Simulating Flux setup (non-main namespace: '${NAMESPACE}')" >&2
+  echo flux bootstrap github \
+    --personal \
+    "--owner=${REPO_OWNER}" \
+    "--repository=${REPO_NAME}" \
+    "--branch=${REPO_BRANCH}" \
+    "--path=${REPO_PATH}" \
+    "--namespace=${NAMESPACE}"
+fi
