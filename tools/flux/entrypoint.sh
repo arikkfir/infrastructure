8190c67178eb256e21cd34627c7ef7148bf0166a
Arik Kfir <arik@kfirs.com>
2021-03-06 15:52:46 +0200

    Fix environment variables validatio in Flux

--- tools/flux/entrypoint.sh
+++ tools/flux/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-[[ -z "${CLUSTER_NAME}" ]] || (echo "CLUSTER_NAME is missing" >&2 && exit 1)
-[[ -z "${CLUSTER_ZONE}" ]] || (echo "CLUSTER_ZONE is missing" >&2 && exit 1)
-[[ -z "${GITHUB_TOKEN}" ]] || (echo "GITHUB_TOKEN is missing" >&2 && exit 1)
-[[ -z "${REPO_OWNER}" ]] || (echo "REPO_OWNER is missing" >&2 && exit 1)
-[[ -z "${REPO_NAME}" ]] || (echo "REPO_NAME is missing" >&2 && exit 1)
-[[ -z "${REPO_BRANCH}" ]] || (echo "REPO_BRANCH is missing" >&2 && exit 1)
-[[ -z "${REPO_PATH}" ]] || (echo "REPO_PATH is missing" >&2 && exit 1)
-[[ -z "${NAMESPACE}" ]] || (echo "NAMESPACE is missing" >&2 && exit 1)
+[[ -z "${CLUSTER_NAME}" ]] && echo "CLUSTER_NAME is missing" >&2 && exit 1
+[[ -z "${CLUSTER_ZONE}" ]] && echo "CLUSTER_ZONE is missing" >&2 && exit 1
+[[ -z "${GITHUB_TOKEN}" ]] && echo "GITHUB_TOKEN is missing" >&2 && exit 1
+[[ -z "${REPO_OWNER}" ]] && echo "REPO_OWNER is missing" >&2 && exit 1
+[[ -z "${REPO_NAME}" ]] && echo "REPO_NAME is missing" >&2 && exit 1
+[[ -z "${REPO_BRANCH}" ]] && echo "REPO_BRANCH is missing" >&2 && exit 1
+[[ -z "${REPO_PATH}" ]] && echo "REPO_PATH is missing" >&2 && exit 1
+[[ -z "${NAMESPACE}" ]] && echo "NAMESPACE is missing" >&2 && exit 1
 [[ "${MODE}" == "apply" ]] || export MODE="preview"
 
 set -euo pipefail
