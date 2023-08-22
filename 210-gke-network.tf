5ea13c923c57fc2a96405dfb38c690ac98f8f2c6
arikkfir <arik@kfirs.com>
2023-08-22 22:59:23 +0300

    fix: create missing GKE VPC firewall rules

--- 210-gke-network.tf
+++ 210-gke-network.tf
@@ -7,14 +7,86 @@ resource "google_compute_network" "default" {
 resource "google_compute_subnetwork" "gke" {
   network       = google_compute_network.default.id
   name          = "gke"
-  ip_cidr_range = "10.2.0.0/16"
+  ip_cidr_range = "10.1.0.0/16"
   region        = "me-west1"
   secondary_ip_range {
     range_name    = "services-range"
-    ip_cidr_range = "192.168.1.0/24"
+    ip_cidr_range = "10.10.0.0/16"
   }
   secondary_ip_range {
     range_name    = "pod-ranges"
-    ip_cidr_range = "192.168.64.0/22"
+    ip_cidr_range = "10.20.0.0/16"
+  }
+}
+
+resource "google_compute_firewall" "default-allow-internal" {
+  name          = "default-allow-internal"
+  network       = google_compute_network.default.name
+  disabled      = false
+  priority      = 65534
+  direction     = "INGRESS"
+  source_ranges = [
+    "10.1.0.0/16",
+    "10.10.0.0/16",
+    "10.20.0.0/16",
+  ]
+
+  allow {
+    protocol = "icmp"
+  }
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
+}
+
+resource "google_compute_firewall" "default-allow-ssh" {
+  name          = "default-allow-ssh"
+  network       = google_compute_network.default.name
+  disabled      = false
+  priority      = 65534
+  direction     = "INGRESS"
+  source_ranges = [
+    "0.0.0.0/0",
+  ]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+}
+
+resource "google_compute_firewall" "default-allow-rdp" {
+  name          = "default-allow-rdp"
+  network       = google_compute_network.default.name
+  disabled      = false
+  priority      = 65534
+  direction     = "INGRESS"
+  source_ranges = [
+    "0.0.0.0/0",
+  ]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3389"]
+  }
+}
+
+resource "google_compute_firewall" "default-allow-icmp" {
+  name          = "default-allow-icmp"
+  network       = google_compute_network.default.name
+  disabled      = false
+  priority      = 65534
+  direction     = "INGRESS"
+  source_ranges = [
+    "0.0.0.0/0",
+  ]
+
+  allow {
+    protocol = "icmp"
   }
 }
