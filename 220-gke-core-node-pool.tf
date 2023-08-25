4086b25aff4efe147703b8bcb26298d7eb052a5a
arikkfir <arik@kfirs.com>
2023-08-25 19:26:54 +0300

    feat(gke): revert back to standard-mode GKE with node pools

---
+++ 220-gke-core-node-pool.tf
@@ -0,0 +1,54 @@
+resource "google_container_node_pool" "core_node_pool" {
+
+  # GENERAL
+  ######################################################################################################################
+  name           = "core"
+  location       = google_container_cluster.main.location
+  cluster        = google_container_cluster.main.name
+  project        = google_container_cluster.main.project
+  node_locations = [local.gcp_zone_a]
+
+  # AUTOSCALING
+  ######################################################################################################################
+  upgrade_settings {
+    max_surge       = 2
+    max_unavailable = 0
+    strategy        = "SURGE"
+  }
+  autoscaling {
+    total_min_node_count = 1
+    total_max_node_count = 3
+    location_policy      = "ANY"
+  }
+
+  # NODE CONFIGURATION
+  ######################################################################################################################
+  node_config {
+    disk_size_gb = 100
+    disk_type    = "pd-standard"
+
+    labels = {
+      "node.kfirs.com/role" = "core"
+    }
+
+    machine_type    = "e2-standard-4"
+    service_account = data.google_compute_default_service_account.default.email
+    preemptible     = false
+    spot            = false
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+
+    workload_metadata_config {
+      mode = "GKE_METADATA"
+    }
+  }
+
+  # OPERATIONS
+  ######################################################################################################################
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+}
