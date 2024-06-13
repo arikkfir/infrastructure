89f37e8193672e755ce1070b1a2915b1fe50d6eb
Arik Kfir <arik@kfirs.com>
2024-06-13 18:33:41 +0300

    dependencies(ci): upgrade Google Cloud & Terraform versions

--- 000-provider.tf
+++ 000-provider.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">=1.5.5"
+  required_version = ">=1.5.7"
   backend "gcs" {
     bucket = "arikkfir-devops"
     prefix = "terraform"
@@ -7,11 +7,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "= 4.78.0"
+      version = "= 5.33.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "= 4.78.0"
+      version = "= 5.33.0"
     }
   }
 }
